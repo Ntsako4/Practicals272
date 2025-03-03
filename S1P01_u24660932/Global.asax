03ef2e3bf67efc947e3f97999895e7d5eb8695ab
ntsak <ntsak@NTSAKO-LAPPYYYY>
2025-03-03 15:43:32 +0200

    Add project files.

---
+++ S1P01_u24660932/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S1P01_u24660932.MvcApplication" Language="C#" %>
---
+++ S1P01_u24660932/Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace S1P01_u24660932.Models
+{
+	public class Class1
+	{
+		[Display(Name = "STUDENT NUMBER")]
+		public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")]
+        public string Name { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string Surname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        public string EmailAddress { get; set; }
+    }
+}
